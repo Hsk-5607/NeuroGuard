447f7de5482bcfc234705cc02921ddddf581198f
Hsk-5607 <hsk050607@gmail.com>
2026-04-22 22:32:14 +0530

    Delete fix_model.py

--- fix_model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-# Load your old model
-model = tf.keras.models.load_model("brain_tumor_model.h5", compile=False)
-
-# Save in new format
-model.save("brain_tumor_model.keras")
-
-print("Model converted successfully!")
